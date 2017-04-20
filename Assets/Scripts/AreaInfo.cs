e7ffd24c08f6c463e6c8dc1e99bfedf147dc8d3d
da1ord <da1ord@home>
2017-04-20 17:38:13 +0200

    Initial commit

---
+++ Assets/Scripts/AreaInfo.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AreaInfo : MonoBehaviour
+{
+    string areaName_;
+    List<GameObject> enemiesList_;
+    SpriteRenderer battleBackground_;
+    bool respawnableArea_;
+
+	// Use this for initialization
+	void Start()
+    {
+        respawnableArea_ = false;
+        
+        // Get battle background image component
+        battleBackground_ = GameObject.Find( "BattleBackgroundImage" ).GetComponent<SpriteRenderer>();
+
+        // Set initial area
+        ChangeArea( "Intro" );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    // Change area on transition
+    public void ChangeArea( string name )
+    {
+        Debug.Log( "Area changed." );
+        areaName_ = name;
+
+        battleBackground_.sprite = Resources.Load<Sprite>( "Battlegrounds/" + name );
+
+        Transform enemies = transform.Find( name + "/Enemies" );
+        enemiesList_ = new List<GameObject>();
+
+        foreach( Transform child in enemies )
+        {
+            if( child.CompareTag( "Enemy" ) )
+            {
+                Debug.Log( child.name + " added." );
+                enemiesList_.Add( child.gameObject );
+                child.gameObject.SetActive( true );
+            }
+        }
+    }
+
+    public void DestroyEnemy( GameObject enemy )
+    {
+        // Check if enemies respawn on area enter
+        if( respawnableArea_ )
+        {
+            // If enemies respawn, hide them
+            enemy.SetActive( false );
+        }
+        else
+        {
+            // If enemies don't respawn, destroy them
+            Destroy( enemy );
+            enemiesList_.Remove( enemy );
+        }
+    }
+
+    public string GetAreaName()
+    {
+        return areaName_;
+    }
+
+    public bool RespawnableArea()
+    {
+        return respawnableArea_;
+    }
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	// Use this for initialization
+	void Start()
+    {
+	}
+	
+	// Update is called once per frame
+	public void MovePosition( Vector3 position )
+    {
+        transform.position = Vector3.Lerp( transform.position, position, 0.1f ) + new Vector3( 0, 0, -10 );	
+	}
+
+    public void BattleCamera()
+    {
+        transform.position = new Vector3( -20.0f, 0.0f, -5.0f );
+
+        // Move camera closer to player
+        Camera.main.orthographicSize = 4;
+    }
+
+    public void MapCamera()
+    {
+        // Move camera further from player
+        Camera.main.orthographicSize = 6;
+    }
+}
