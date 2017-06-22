2749c090aaf5a66b42516887201b443ad70be7d0
da1ord <da1ord@home>
2017-06-22 17:41:28 +0200

    Revert "Added damage rendering"
    This reverts commit f97a792d0823e33d61c2092abbb9a88c1dcec4c5.

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -79,59 +78,3 @@ public class PlayerHealth : MonoBehaviour
         return health_;
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-public class PlayerHealth : MonoBehaviour
-{
-    // Player's max health
-    int maxHealth_ = 100;
-    // Player's actual health
-    int health_ = 100;
-    
-    // Health text
-    public Text healthText_;
-
-    // Use this for initialization
-    void Start()
-    {	
-	}
-	
-	// Update is called once per frame
-	void Update()
-    {
-    }
-    
-    // Player hurt function
-    public void TakeDamage( int damage )
-    {
-        // Player is alive
-        if( health_ - damage > 0 )
-        {
-            // Decrease the health, update healt slider and the text
-            health_ -= damage;
-            healthText_.text = health_.ToString() + "/" + maxHealth_.ToString();
-
-        }
-        // Player is dead
-        else
-        {
-            // Set health to 0, update healt slider and the text
-            health_ = 0;
-            healthText_.text = health_.ToString() + "/" + maxHealth_.ToString();
-
-            // Start death routine
-            //StartCoroutine( Death() );
-        }
-    }
-
-    // Player health getter
-    public int GetHealth()
-    {
-        return health_;
-    }
-}
->>>>>>> b26a507af31452553c716ea355cbdf2c1484bb42
